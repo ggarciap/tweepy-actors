68c745f1ef272f2064476def499c269ab2d73770
Genaro Garcia Pereyra <ggp@Genaros-MacBook-Pro-3.local>
2021-01-12 20:28:24 -0500

    updated + addition of classes for individuals

--- streamer.py
+++ streamer.py
@@ -1,26 +1,51 @@
+from tweepy import API
+from tweepy import Cursor
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 import creds
 
+# # # # CLIENT # # # #
+class Tclient():
+    
+    def __init__(self,twitter_user=None):
+        self.auth = TAuthenticator.authenticate_twitter_app()
+        self.twitter_client = API(self.auth)
+        self.twitter_user = twitter_user
+
+    def get_user_timeline_tweets(self, num_tweets=10):
+        tweets = list()
+        for tweet in Cursor(self.twitter_client.user_timeline, id=self.twitter_user).items(num_tweets):
+            tweets.append(tweet)
+        return tweets
+
+class TAuthenticator():
+    
+    @staticmethod
+    def authenticate_twitter_app():
+        auth = OAuthHandler(creds.CONSUMER_KEY, creds.CONSUMER_SECRET)
+        auth.set_access_token(creds.ACCESS_TOKEN, creds.ACCESS_TOKEN_SECRET)
+        return auth
+
 class TwitterStreamer():
     '''
     Class for processing plus streaming
     '''
+    # def __init__(self):
+    #     self.twitter_authenticator = TAuthenticator()
     def stream_tweets(self, fetched_tweets_filename, individuals):
         # Handles Twitter connection and connection to Twitter Streaming API
-        listener = stdOutListener(fetched_tweets_filename)
-        auth = OAuthHandler(creds.CONSUMER_KEY, creds.CONSUMER_SECRET)
-        auth.set_access_token(creds.ACCESS_TOKEN, creds.ACCESS_TOKEN_SECRET)
-
+        listener = TListener(fetched_tweets_filename)
+        auth = TAuthenticator.authenticate_twitter_app()
         stream = Stream(auth, listener)
 
         stream.filter(track= individuals)
 
 
 
-
-class stdOutListener(StreamListener):
+# Passing reference of the Class StreamListener
+# Inheretance
+class TListener(StreamListener):
     '''
     Basic listener class
     '''
@@ -37,12 +62,17 @@ class stdOutListener(StreamListener):
         return True
 
     def on_error(self, data):
+        if status == 420:
+            'In case rate limit occurs'
+            return False
         print(status)
 
 if __name__ == "__main__":
     ind_list = 'donald trump', 'barack obama', 'AOC'
     ftf_name = 'tweets.json'
 
-    t_streamer = TwitterStreamer()
-    t_streamer.stream_tweets(ftf_name, ind_list)
+    twitter_c = Tclient('elonmusk')
+    print(twitter_c.get_user_timeline_tweets(1))
+    # t_streamer = TwitterStreamer()
+    # t_streamer.stream_tweets(ftf_name, ind_list)
 
